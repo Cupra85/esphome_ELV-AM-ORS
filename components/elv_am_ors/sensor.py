84a0717649ca02a347907690c6b922b1d4f11cdc
Cupra85 <daniel.curten@gmail.com>
2026-01-02 14:54:23 +0100

    Add configuration constants and update sensor code

--- components/elv_am_ors/sensor.py
+++ components/elv_am_ors/sensor.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import STATE_CLASS_MEASUREMENT
+from esphome.const import CONF_ID, STATE_CLASS_MEASUREMENT
 from esphome import pins
 
+# Sensor keys
 CONF_UVA = "uva"
 CONF_UVB = "uvb"
 CONF_UVC = "uvc"
@@ -12,9 +13,11 @@ CONF_ILLUMINANCE = "illuminance"
 CONF_IRRADIANCE = "irradiance"
 CONF_IRRA_ADC_PIN = "irra_adc_pin"
 
+# Namespace
 elv_ns = cg.esphome_ns.namespace("elv_am_ors")
 ELVAMORS = elv_ns.class_("ELVAMORS", cg.PollingComponent, i2c.I2CDevice)
 
+# Configuration schema
 CONFIG_SCHEMA = (
     cv.Schema(
         {
@@ -50,9 +53,10 @@ CONFIG_SCHEMA = (
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
 
+            # ADC pin for irradiance (default GPIO3)
             cv.Optional(
                 CONF_IRRA_ADC_PIN,
-                default="GPIO3"
+                default="GPIO3",
             ): pins.internal_gpio_input_pin_schema,
         }
     )
@@ -60,15 +64,19 @@ CONFIG_SCHEMA = (
     .extend(i2c.i2c_device_schema(0x77))
 )
 
+# Code generation
 async def to_code(config):
-    var = cg.new_Pvariable(config[cg.CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
+
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
+    # ADC pin
     if CONF_IRRA_ADC_PIN in config:
         pin = await cg.gpio_pin_expression(config[CONF_IRRA_ADC_PIN])
         cg.add(var.set_irra_adc_pin(pin))
 
+    # Optional sensors
     for key in (
         CONF_UVA,
         CONF_UVB,
